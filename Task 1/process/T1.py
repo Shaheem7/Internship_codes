56fdb0900fadd9b25997dd0334760f24b320bf64
Shaheem <shaheembutt11@gmail.com>
2024-06-10 09:27:20 +0530

    First commit

---
+++ Task 1/process/T1.py
@@ -0,0 +1,119 @@
+###############################
+# Import Python modules
+###############################
+import os, sys, datetime, inspect
+
+###############################
+# Import ASN1 modules
+###############################
+from pyasn1.codec.ber import decoder
+from pyasn1.codec.ber import encoder
+from pyasn1.type import char
+from pyasn1.type import tag
+from pyasn1.type import univ
+
+###############################
+# Import Scapy and Goose Modules
+###############################
+# We have to tell script where to find the Goose module in parent directory
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from scapy.layers.l2 import Ether
+from scapy.layers.l2 import Dot1Q
+from scapy.compat import raw
+from scapy.all import rdpcap
+from goose.goose import GOOSE
+from goose.goose import GOOSEPDU
+from goose.goose_pdu import AllData
+from goose.goose_pdu import Data
+from goose.goose_pdu import IECGoosePDU
+from goose.goose_pdu import UtcTime
+
+
+
+###############################
+# Import packets into SCAPY
+###############################
+# inf = sys.argv[1]
+inf = 'goose.pcapng'
+packets = rdpcap(inf)
+
+###############################
+# Identify packets containing GOOSE messages. 
+# Sometimes these will be hidden within VLAN packets, so account for these
+###############################
+
+GOOSE_TYPE = 0x88b8
+def gooseTest(pkt):
+    isGoose = False
+    # Test for a Goose Ether Type
+    if pkt.haslayer('Dot1Q'):
+        if pkt['Dot1Q'].type == GOOSE_TYPE: isGoose = True
+    if pkt.haslayer('Ether'):
+        if pkt['Ether'].type == GOOSE_TYPE: isGoose = True
+    return isGoose
+
+
+###############################
+# Process GOOSE PDU by decoding it with PYASN1
+###############################
+def goose_pdu_decode(encoded_data):
+
+    g = IECGoosePDU().subtype(
+        implicitTag=tag.Tag(
+            tag.tagClassApplication,
+            tag.tagFormatConstructed,
+            1
+        )
+    )
+    decoded_data, unprocessed_trail = decoder.decode(
+        encoded_data,
+        asn1Spec=g
+    )
+    # This should work, but not sure.
+    return decoded_data
+
+###############################
+# Process packets and search for GOOSE
+###############################
+# datasets = {src_mac:{'gocbref':GoCBRef, 'dataset':DataSet, 'goid':GoID}}
+datasets = {}
+for p in packets:
+    # Only process Goose Packets
+    if gooseTest(p):
+        # Use SCAPY to parse the Goose header and the Goose PDU header
+        d = GOOSE(p.load)
+
+        # Grab the Goose PDU for processing
+        gpdu = d[GOOSEPDU].original
+
+        # Use PYASN1 to parse the Goose PDU
+        gd = goose_pdu_decode(gpdu)
+
+        src_mac = p['Ether'].src
+        gocbref = str(gd['gocbRef'])
+        dataset = str(gd['datSet'])
+        goid    = str(gd['goID'])
+        numDatSetEntries = int(gd['numDatSetEntries'])
+        godata = '%s - %s - %s - %s'%(gocbref, dataset, goid, numDatSetEntries)
+        if src_mac in datasets.keys():
+            if godata not in datasets[src_mac]:
+                datasets[src_mac].append(godata)
+        else:
+            datasets[src_mac] = [godata]
+
+###############################
+# Print Statements and Functions
+###############################
+## Normal Print Statement
+print('Goose Data by Device Hardware Address')
+indent = '    '
+for src in datasets.keys():
+    print('Source Device: %s'%(src))
+    for e in datasets[src]:
+        print('%s%s'%(indent,e))
+
+
+print(datasets)
---
+++ Task 1/process/test.py
@@ -0,0 +1,27 @@
+import random
+g_packets = 2201
+pnum = random.randint(0,g_packets)
+
+print(pnum)
+
+
+
+t = 2
+
+list2d = [[1,2], [46, 56],[34, 93]]
+
+s, d = list2d[t]
+
+print(s)
+print(d)
+
+
+if 4 in range(6):
+    print("Working")
+    
+    
+inputs = list(map(int, input("Enter multiple integer values separated by space: ").split()))
+print(inputs)
+
+print(type(inputs))
+print(type(inputs[1]))
